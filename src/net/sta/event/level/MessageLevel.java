b67bf454e107fab5197f3394d7589f803fa71df6
Jan <86261346+Salzstange-app@users.noreply.github.com>
2024-04-18 16:51:51 +0200

    Code aufgeräumt

--- src/net/sta/event/level/MessageLevel.java
+++ src/net/sta/event/level/MessageLevel.java
@@ -1,17 +1,19 @@
 package net.sta.event.level;
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.sta.event.listener.EventAdapter;
+import net.sta.event.message.MessageReceivedEvent;
 import net.sta.managers.XpManager;
+import org.jetbrains.annotations.NotNull;
 
 
-public class MessageLevel extends ListenerAdapter implements net.sta.event.level.Manager.MessageLevel, XpManager {
+public class MessageLevel extends EventAdapter implements net.sta.event.level.Manager.MessageLevel, XpManager {
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        if (!event.isFromGuild() && event.getAuthor().isBot())return;
-        if (canGetMessageXp(event.getMember())){
-            setMessagePlayerTime(event.getMember(), 10);
-            randXp(event.getMember(), 10);
+    public void onMessageReceivedEvent(@NotNull MessageReceivedEvent event) {
+        if (event.getMessage().getAuthor().isBot())return;
+        if (canGetMessageXp(event.getMessage().getMember())){
+            setMessagePlayerTime(event.getMessage().getMember(), 10);
+            randXp(event.getMessage().getMember(), 10);
         }
     }
 }
